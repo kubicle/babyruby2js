7b76bd308d114f6561e24fdf60a1426d5a623517
kubicle <kubicle@yahoo.com>
2015-03-03 22:26:57 +0900

    Fixed "module.exports=" for class decl + path for require

--- babyruby2js.rb
+++ babyruby2js.rb
@@ -1,7 +1,9 @@
-# TODO:
-# - find comments that were not used and dump them too
-# - count method on Array
 # Not handled:
+# - generated "require" need review
+# - class constants need to be moved *after* the constructor
+# - constants added to main class need to be "required" at least once
+# - we could find comments that were not used and dump them too
+# - "count" method on Array
 # - array.size not translated as "length"
 # - next & return not translated right in callbacks
 # - if x = f() changed into if (x = f()) not liked by JSHint; but if (x=f()) is OK
@@ -15,12 +17,13 @@
 # - object.rw_attrib=(exp); the method is "rw_attrib=" and it works by chance, no issue...
 # - break(value)
 # - retry exception
+# - cannot have parameters with a default value for a method using yield (block)
 # - iterators returned by each, upto, step, etc.
 # - in "case" (switch) a break is generated even if last stmt is return or throw (JSHint complains)
 # - map[:key] => map['key'] instead of map.key (JSHint complains)
 # - .size() called on arrays is not changed into .length
-# - constants added to main class
 # - 0 and "" are true in Ruby and false in JS, hence code like if !a.find_index(...) needs work
+# - negative index on arrays does not "loop back" from last item
 
 require 'trollop'
 require 'parser/current'
@@ -529,7 +532,7 @@ class RubyToJs
     after = ";"
     if methName == "initialize"
       proto = "#{cr}/** @class */#{cr}function #{@class}("
-      after = "#{cr}exports = #{@class};"
+      after = "#{cr}module.exports = #{@class};"
       parent = @curClass[:parent]
       if parent
         after = "#{cr}inherits(#{@class}, #{parent});" + after
@@ -607,7 +610,7 @@ class RubyToJs
     mod = exp(n)
     mod = mod[1..-2] if mod.start_with?("'")
     slash = mod.rindex("/")
-    path = slash ? mod[0..slash] : ""
+    path = slash ? mod[0..slash] : "./"
     mod = slash ? mod[slash+1..-1] : mod
     className = classNameFromFileName(mod)
     @dependencies[className] = false # no need to generate again
