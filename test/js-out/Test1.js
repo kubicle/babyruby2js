f8566a2d719b3f24b68862f3dc116b5cdc68d83e
kubicle <kubicle@yahoo.com>
2015-03-21 19:09:35 +0900

    Update Test1 - WIP

--- test/js-out/Test1.js
+++ test/js-out/Test1.js
@@ -2,8 +2,13 @@
 'use strict';
 
 // comm1
-TestStone.prototype.f1 = function (a) { // f1 decoring comment
-    return a.slice(1, 4) + a.range(-1, -4);
+TestStone.prototype.test1 = function (a) { // f1 decoring comment
+    for (var n, n_array = a, n_ndx = 0; n=n_array[n_ndx], n_ndx < n_array.length; n_ndx++) {
+        p(a.slice(1, 4) + a.range(-1, -4));
+    }
+    return a.block_fn(function (x) { // block arg x comment
+        return p(x);
+    });
 };
 
 // bug2.1
