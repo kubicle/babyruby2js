a055d1fdc85a8cbe6037fc77b9082b680afce7c5
kubicle <kubicle@yahoo.com>
2015-03-22 22:48:05 +0900

    New test3

---
+++ test/js-out/Test3.js
@@ -0,0 +1,23 @@
+//Translated from test3.rb using babyruby2js
+'use strict';
+
+// Class comment
+// another class comment
+// attr_accessor comment
+attr_accessor('foo');
+// method comment
+Foo.prototype.bar = function () {
+    // expr comment
+    return 1 + 2; // intermediate comment // stray comment
+};
+
+main.prototype.foo = function () {
+    if (true) {
+        p('hi1');
+        if (x() < 0) { // deco on trailing if
+            throw new Error(0);
+        }
+    } else {
+        return p('hi2');
+    }
+};
---
+++ test/test3.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/env ruby
+# coding: utf-8
+# Class comment
+# another class comment
+class Foo
+  # attr_accessor comment
+  attr_accessor :foo
+
+  # method comment
+  def bar
+    # expr comment
+    1 + # intermediate comment
+      2
+    # stray comment
+  end
+end
+
+def foo
+  if true
+    p "hi1"
+    raise 0 if x<0 # deco on trailing if
+  else
+    p "hi2"
+  end
+end
+
+# (if
+#   (true)
+#   (begin
+#     (send nil :p
+#       (str "hi1"))
+#     (if
+#       (send
+#         (send nil :x) :<
+#         (int 0))
+#       (send nil :raise
+#         (int 0)) nil))
+#   (send nil :p
+#     (str "hi2")))
