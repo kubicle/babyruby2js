150465705a445c2efa6443e46b47c3d7190818e4
kubicle <kubicle@yahoo.com>
2015-03-24 01:22:00 +0900

    updated test1 - WIP on param and block comments

--- test/js-out/Test1.js
+++ test/js-out/Test1.js
@@ -2,13 +2,25 @@
 'use strict';
 
 // comm1
-TestStone.prototype.test1 = function (a) { // f1 decoring comment
-    for (var n, n_array = a, n_ndx = 0; n=n_array[n_ndx], n_ndx < n_array.length; n_ndx++) { // block arg n comment
-        p(a.slice(1, 4) + a.range(-1, -4));
+TestStone.prototype.testDeco1 = function () {
+    if (true) {
+        p('hi');
+        if (x() < 0) { // deco on trailing if
+            throw new Error(0);
+        }
+    } else {
+        return p('hi');
     }
-    return a.block_fn(function (x) { // block arg x comment
+};
+
+TestStone.prototype.test1 = function (a // test1 decoring comment
+) {
+    for (var n, n_array = a, n_ndx = 0; n=n_array[n_ndx], n_ndx < n_array.length; n_ndx++) {
+        p(a.slice(1, 4) + a.range(-1, -4)); // block arg n comment
+    }
+    return a.block_fn(function (x) {
         return p(x);
-    });
+    }); // block arg x comment
 };
 
 // bug2.1
